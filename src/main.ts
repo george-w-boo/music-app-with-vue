562234024908b754d889a1f0156550afb3370ad9
Mykhailo Ostapysko <mykhailo.ostapysko@gmail.com>
2022-06-02 10:41:32 +0300

    adds vuex basic configuration

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+// import router
+import { store } from "./store/index";
 import "./assets/base.css";
+import { key } from "./store";
 
 const app = createApp(App);
 
+app.use(store, key);
 app.mount("#app");
