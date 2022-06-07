79e861bf333b1146c5cf73008a6b2ae2eb7fd51a
Mykhailo Ostapysko <mykhailo.ostapysko@gmail.com>
2022-06-07 10:27:42 +0300

    adds upload handler method

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -15,7 +15,7 @@
         @dragover.prevent.stop="isDragOver = true"
         @dragenter.prevent.stop="isDragOver = true"
         @dragleave.prevent.stop="isDragOver = false"
-        @drop.prevent.stop="upload"
+        @drop.prevent.stop="upload($event)"
       >
         <h5>Drop your files here</h5>
       </div>
@@ -55,6 +55,7 @@
 </template>
 
 <script>
+import { storage } from "@/includes/firebase";
 export default {
   name: "Upload",
   data() {
@@ -63,8 +64,22 @@ export default {
     };
   },
   methods: {
-    upload() {
+    upload($event) {
       this.isDragOver = false;
+
+      const files = [...$event.dataTransfer.files];
+
+      console.log("event.files", files);
+
+      files.forEach((file) => {
+        if (file.type !== "audio/mpeg") {
+          return;
+        }
+
+        const storageRef = storage.ref();
+        const songsRef = storageRef.child(`songs/${file.name}`);
+        songsRef.put(file);
+      });
     },
   },
 };
